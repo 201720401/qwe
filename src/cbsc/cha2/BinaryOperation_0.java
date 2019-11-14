0a971e6c27de42a500f12fa9f17fa0a4233eb0b5
201720401 <2478374752@qq.com>
2019-11-14 13:16:44 +0800

    text1

--- src/cbsc/cha2/BinaryOperation_0.java
+++ src/cbsc/cha2/BinaryOperation_0.java
@@ -2,10 +2,10 @@ package cbsc.cha2;
 import java.util.Random;
 public class BinaryOperation_0 {
 	public static void main(String[] args) {
-		short m=0,n=0,ov=0,q=0;
+		short m=0,n=0,ov=0;
 		char o='+';
 		Random random=new Random();
-		for(int i=0;i<200;i++)
+		for(int i=0;i<50;i++)
 		{
 			ov=(short)random.nextInt(2);
 			m=(short)random.nextInt(101);
@@ -13,35 +13,12 @@ public class BinaryOperation_0 {
 			if(ov==1)
 			{
 				o='+';
-				if(m+n>100)
-					continue;
 			}
 			else
 			{
 				o='-';
-				if(m-n<0)
-					continue;
 			}
-			q++;
-			if(q>50)
-				continue;
-			if((q-1)%5==0)
-				System.out.println();
-			System.out.print(""+q+":\t"+m+o+n+"=");
-			Aery(m,n,ov);
+			System.out.println(""+(i+1)+":\t"+m+o+n+"=");
 		}
 	}
-	public static void Aery(short m,short n,short ov) 
-	{
-		int p;
-		if(ov==1){
-			p=m+n;
-		}
-		else{
-			p=m-n;
-		}
-		System.out.print(p);
-		System.out.print("  ");
-	}
-
 }
